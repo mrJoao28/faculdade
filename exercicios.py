e2c87d9d1c3c6e291d160e76b5ffc0cfa4f126ef
mrjhon <joaofelipeazevedo28@gmail.com>
2026-03-31 07:09:57 -0400

    modified:   exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -20,3 +20,11 @@ def quantas_ferraduras():
     cavalos  = int(input("QUantos cavalos tem? : "))
     return "Serão necessarios {} ferraduras".format(cavalos*4)
 
+def carro():
+    capacidade_maxima = float(input("QUal é a capacidade do tanque? : "))
+    litros_abastecidos = float(input("QUantos litros foram abastecidos? : "))
+    kilometros_percorridos = float(input("Quantos quilometors foram percorridos?: "))
+    performace = kilometros_percorridos/litros_abastecidos
+    resto = (capacidade_maxima-litros_abastecidos)*performace
+    return "O carro tem uma performace de {} km/l e poderia percorrer mais {} km".format(performace , resto)
+
